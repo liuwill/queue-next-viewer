0a1bbb1066f26dea4936dd0ec4fb1d93694073e5
liuwill <liuwei_will@qq.com>
2018-06-25 14:15:37 +0800

    make package start right

--- next.config.js
+++ next.config.js
@@ -52,6 +52,17 @@ module.exports = {
       }
     )
 
+    if (dev) {
+      config.module.rules.push({
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          // eslint options (if necessary)
+        }
+      })
+    }
+
     return config
   }
 }
